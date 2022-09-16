5c5b2447a6736a96ce80f38ef9e236ea1114d072
mohammedyahyaa <my212634@gmail.comd>
2022-09-16 18:40:56 +0200

    add 006

---
+++ week 1/006/script.js
@@ -0,0 +1,9 @@
+
+//just comment the code using these two ways 
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+
+/*console.log("Iam In Console");
+document.write("Iam In Page");*/
