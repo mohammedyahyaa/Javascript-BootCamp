733673efd4cbaab5aa0a456c295c409b42a26cc5
mohammedyahyaa <my212634@gmail.comd>
2022-09-16 17:56:56 +0200

    add 002

---
+++ week 1/002/script.js
@@ -0,0 +1,19 @@
+
+//create h1 
+document.write("<h1 id='main'>ElZero</h1>") ;
+
+// apply css
+document.querySelector("h1").style.color="blue";
+document.querySelector("h1").style.fontSize="80px";
+document.querySelector("h1").style.fontFamily="Arial";
+document.querySelector("h1").style.fontWeight="Bold";
+document.getElementById("main").style.textAlign = "center";
+
+
+
+
+
+
+
+
+
