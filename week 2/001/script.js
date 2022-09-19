515fdd01cc45cacea12b9819fad23bdb202ef0f4
mohammedyahyaa <my212634@gmail.comd>
2022-09-19 08:31:05 +0200

    add week 002 first assignment

---
+++ week 2/001/script.js
@@ -0,0 +1,11 @@
+
+
+var x =10 ;
+var y =20 ;
+
+
+console.log(x+""+y);             //Normal concatenate =>1020
+console.log(typeof(x+""+y));     // Normal concatenate => string
+console.log(`${x}${y}`);         //literals way  => 1020
+console.log(typeof(`${x}${y}`));  // literals way => string
+
