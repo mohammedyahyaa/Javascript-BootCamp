b680d31bea96b266e4b7da64d5b4312765415b35
mohammedyahyaa <my212634@gmail.comd>
2022-09-20 14:56:19 +0200

    add week three assignments

---
+++ week 3/main.js
@@ -0,0 +1,58 @@
+
+
+// ****assignment 1 
+ 
+console.log(10 * 20 + (15 % 3) + 190 + 10 - 400);   // 0
+
+
+
+// ****assignment 2 
+
+let num = 3; 
+
+//solution one 
+console.log(num + +true + --num); 
+
+// Solution two
+console.log((num++ * num--) / --num); 
+
+//solution three 
+console.log(num*num-num);
+
+// Soultion Four
+console.log(num+num);
+
+// Solution Five
+console.log(num ** num + num++); 
+
+
+//solution six 
+console.log(num+=num);
+
+
+
+// ****assignment three 
+
+
+let number = "10" ;
+
+
+// solution one 
+console.log(+number + +number); //20
+
+//solution two 
+console.log (Number(number)+Number(number)); //20 
+
+//solution three 
+console.log(parseInt(number)+parseInt(number));
+
+
+// *** assigmnet four
+
+let points = 10 ;
+
+
+console.log(++points+true+true);              // 13
+
+console.log(--points-true) ;                  //8
+
